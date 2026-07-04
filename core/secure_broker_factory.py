d1a9def2b4f48984a22c39daed54769e2b2ca5f9
alanua <alanua@users.noreply.github.com>
2026-07-04 20:14:06 +0200

    Fail closed for unbounded executors

--- core/secure_broker_factory.py
+++ core/secure_broker_factory.py
@@ -4,14 +4,12 @@ from core.isolated_process_executor import isolated_local_process_executor
 from core.runner_execution_broker import (
     RunnerExecutionBroker,
     composite_executor_factory,
-    network_http_executor,
 )
 from core.runner_executors import (
     ExecutionContext,
     filesystem_executor,
     python_entrypoint_executor,
     remote_ssh_executor,
-    repository_executor,
 )
 
 
@@ -20,10 +18,8 @@ def secure_default_broker(context: ExecutionContext) -> RunnerExecutionBroker:
         {
             "local.process": isolated_local_process_executor,
             "remote.ssh": remote_ssh_executor(context),
-            "network.http": network_http_executor,
             "python.entrypoint": python_entrypoint_executor(context),
             "filesystem": filesystem_executor(context),
-            "repository": repository_executor(context),
         }
     )
     broker._executors["composite"] = composite_executor_factory(broker)
